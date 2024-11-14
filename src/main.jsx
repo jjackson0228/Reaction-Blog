18ed53802aa9742ec6a4c82f41107c3a6f9c5b84
James Jackson <jjackson0228@gmail.com>
2024-11-14 18:40:14 -0500

    got the app to show up with my modifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {
+  createBrowserRouter,
+  // redirect,
+  RouterProvider,
+} from 'react-router-dom';
+
 import App from './App';
-import './index.css'; // Import Tailwind CSS
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
